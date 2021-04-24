0385fc2207667e346d3d511d6270e2517e22538a
Felix <felixcpiercy@gmail.com>
2021-04-24 09:37:10 +0100

    Refactored the interactice menu

--- directory.rb
+++ directory.rb
@@ -1,26 +1,25 @@
+@students = [] # an empty array accessible to all methods
+
 def input_students
   puts 'Please enter the names of the students'
   puts 'To finish, just hit return twice'
-  # create an empty array
-  students = []
   # get the first name
   puts 'Enter the Villain'
   name = gets.chomp
   # while the name if not empty, repeat this code
   until name.empty?
-    # add the student has to the array
-    students << { name: name, cohort: :april}
-    if students.length == 1
-      puts "Now we have #{students.count} great student".center(30)
+    # add the student hash to the array
+    @students << { name: name, cohort: :april }
+    if @students.length == 1
+      puts "Now we have #{@students.count} great student".center(30)
     else
-      puts "Overall, we have #{students.count} great students".center(30)
-      # get another name from the user
+      puts "Overall, we have #{@students.count} great students".center(30)
+
     end
+    # get another name from the user
     puts 'Enter the Villain'
     name = gets.chomp
   end
-  # return array of students
-  students
 end
 
 def print_header
@@ -28,38 +27,50 @@ def print_header
   puts '-------------'.center(30)
 end
 
-def print_footer(students)
-  if students.length == 1
-    puts "Overall, we have #{students.count} great student".center(30)
+def print_students_list
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer
+  if @students.length == 1
+    puts "Overall, we have #{@students.count} great student".center(30)
   else
-    puts "Overall, we have #{students.count} great students".center(30)
+    puts "Overall, we have #{@students.count} great students".center(30)
+  end
+end
+
+def print_menu
+  puts '1. Input the students'
+  puts '2. Show the students'
+  puts '9. Exit' # we will add more options
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when '1'
+    input_students
+  when '2'
+    show_students
+  when '9'
+    exit
+  else
+    puts 'Unknown input. Please try again.'
   end
 end
 
 def interactive_menu
-  students = []
   loop do
-    # 1 print the menu and ask the user what to do
-    puts '1. Input the students'
-    puts '2. Show the students'
-    puts '9. Exit'
-    # 2 read the input and save it into a variable
-    selection = gets.chomp
-    # 3 do what the user has asked
-    case selection
-    when '1'
-      students = input_students
-    when '2'
-      print_header
-      print(students)
-      print_footer(students)
-    when '9'
-      exit # this will terminate the programme
-    else
-      puts 'Unknown input. Please try again.'
-    end
+    print_menu
+    process(gets.chomp)
   end
-  # method end, dont use
 end
 
 interactive_menu
